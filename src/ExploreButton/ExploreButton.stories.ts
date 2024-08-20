11ee3b23deafb6a4aa5c188ec193386bdc683c6b
Jakub Chojnacki <jakub.chojnacki27@gmail.com>
2024-08-20 18:12:53 +0200

    remove useless import in ExploreButton

--- src/ExploreButton/ExploreButton.stories.ts
+++ src/ExploreButton/ExploreButton.stories.ts
@@ -2,8 +2,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { fn } from "@storybook/test";
 import ExploreButton from "./ExploreButton";
 import "../index.css";
-import { background } from "storybook/internal/theming";
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+
 const meta = {
   title: "ExploreButton",
   component: ExploreButton,
